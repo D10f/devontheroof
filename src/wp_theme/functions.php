d99c88bda63a4afef07693dd7a9bb4bd1613227d
herokunt <6wdcd9q17d86.scruff@simplelogin.co>
2021-05-26 16:01:53 +0200

    small fix in mobile view

--- src/wp_theme/functions.php
+++ src/wp_theme/functions.php
@@ -19,7 +19,7 @@ remove_action('admin_print_styles', 'print_emoji_styles');
 function load_scripts_and_styles() {
   wp_enqueue_script('searchbar', get_theme_file_uri('/js/postLoader.js'), NULL, '1.0', true);
   wp_enqueue_script('content_table', get_theme_file_uri('/js/contentTable.js'), NULL, '1.0', true);
-  wp_enqueue_style('main_css', get_theme_file_uri('/css/main.da7428cb082976469436.css'));
+  wp_enqueue_style('main_css', get_theme_file_uri('/css/main.2aa90bc66ebb0afa87bf.css'));
 }
 
 // Sets up event a custom endpoint: /content/posts
