e67ed493a02efd797614b21ce602861da0da3396
herokunt <6wdcd9q17d86.scruff@simplelogin.co>
2021-07-19 21:11:19 +0200

    updates on styles

--- src/wp_blog/theme/functions.php
+++ src/wp_blog/theme/functions.php
@@ -21,7 +21,7 @@ function load_scripts_and_styles() {
   wp_enqueue_script('content_table', get_theme_file_uri('/js/contentTable.js'), NULL, '1.0', true);
   wp_enqueue_script('syntax_highlighter', get_theme_file_uri('/js/prism.js'), NULL, '1.0', true);
   wp_enqueue_style('syntax_highlighter', get_theme_file_uri('/css/prism.css'));
-  wp_enqueue_style('main_css', get_theme_file_uri('/css/main.fbab573952da9eb7f79d.css'));
+  wp_enqueue_style('main_css', get_theme_file_uri('/css/main.6d407cf40d8e28f0ff08.css'));
 }
 
 // Sets up event a custom endpoint: /content/posts
