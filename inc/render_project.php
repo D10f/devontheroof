27a91b0edebe6a9ce2f6ae644f3b00a141d16dc0
herokunt <galactodensimeter_bismuthyl@simplelogin.fr>
2022-06-07 22:51:31 +0200

    Add disabled button to projects without a project link. Updated tooltip positions

--- inc/render_project.php
+++ inc/render_project.php
@@ -69,6 +69,10 @@ function render_project($project_section, $projects) {
             <footer class="project__footer">
               <?php if ($project['project_link']) { ?>
                 <a class="btn" href="<?= $project['project_link'] ?>" target="_blank" rel="noreferrer">View</a>
+              <?php } else if ( $project['project_type'] == 'webapp' AND ! $project['project_link'] ) { ?>
+                <span class="tooltip tooltip--top-right" data-tooltip="Down for maintainance">
+                  <button class="btn" disabled>View</button>
+                </span>
               <?php } ?>
               
               <?php if ($project['repo_link']) { ?>
@@ -80,7 +84,7 @@ function render_project($project_section, $projects) {
 
           <ul class="project__stack">
             <?php foreach($project['tech_stack'] as $tech) { ?>
-              <li class="tooltip" data-tooltip="<?= $tech['label']  ?>">
+              <li class="tooltip tooltip--bottom" data-tooltip="<?= $tech['label']  ?>">
                 <svg aria-hidden="true">
                   <use xlink:href="<?= get_theme_file_uri('assets/images/sprite.svg') . "#icon-{$tech['value']}" ?>"></use>
                 </svg>
