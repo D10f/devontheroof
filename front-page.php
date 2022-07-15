68add14a793c33c693d1e273175f93823f13f993
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2022-07-15 21:57:42 +0200

    removed floating arrow navigation link. Refactor link to blog to show border and bit of cleanup.

--- front-page.php
+++ front-page.php
@@ -1,83 +1,81 @@
 <?php get_header(); ?>
 
-  <header class="landing__header">
+<header class="landing__header">
 
-    <!-- Introduction -->
-    <section class="landing__introduction">
-      <?php the_content(); ?>
-      <a
-        class="btn--round btn--large btn--light animate-hover animate-pulse hide-on-tablet"
-        href="#web-applications"
-        aria-label="hidden"
-      >
-        &darr;
-      </a>
-    </section>
+  <!-- Introduction -->
+  <section class="landing__introduction">
+    <?php the_content(); ?>
+    <!--
+    <a class="btn--round btn--large btn--light animate-hover animate-pulse hide-on-tablet" href="#web-applications" aria-label="hidden">
+      &darr;
+    </a>
+    -->
+  </section>
 
 
-    <!-- Recent blog entries -->
-    <section class="landing__recent-posts">
-      <a class="landing__posts-separator animate-pulse" href="<?= site_url('blog'); ?>">BLOG</a>
-      <?php get_template_part('templates/recent_posts'); ?>
-    </section>
+  <!-- Recent blog entries -->
+  <section class="landing__recent-posts">
+    <a class="landing__posts-separator" href="<?= site_url('blog'); ?>">BLOG</a>
+    <?php get_template_part('templates/recent_posts'); ?>
+  </section>
 
-  </header>
+</header>
 
-  <!-- Projects By Type -->
-  <main>
-    <?php
+<!-- Projects By Type -->
+<main>
+  <?php
 
-    $projects = new WP_Query([
-      'post_type' => 'project',
-    ]);
-    
+  $projects = new WP_Query([
+    'post_type' => 'project',
+  ]);
 
-    $webapp_projects = [];
-    $graphics_projects = [];
-    $script_projects = [];
-    $devops_projects = [];
 
-    while($projects->have_posts()) {
-      $projects->the_post();
-      $project_type = get_field('project_type');
+  $webapp_projects = [];
+  $graphics_projects = [];
+  $script_projects = [];
+  $devops_projects = [];
 
-      $post_data = [
-        "id" => get_the_ID(),
-        "name" => get_the_title(),
-        "content" => get_the_content(),
-        "project_type" => $project_type,
-        "project_link" => get_field('project_link'),
-        "repo_link" => get_field('repo_link'),
-        "tech_stack"=> get_field('tech_stack'),
-        "sketch" => get_field('sketch')
-      ];
+  while ($projects->have_posts()) {
+    $projects->the_post();
+    $project_type = get_field('project_type');
 
-      switch ($project_type) {
-        case 'webapp':
-          array_push($webapp_projects, $post_data);
-          break;
-        case 'graphics':
-          array_push($graphics_projects, $post_data);
-          break;
-        case 'scripts':
-          array_push($script_projects, $post_data);
-          break;
-        case 'devops':
-          array_push($devops_projects, $post_data);
-          break;
-        default:
-          break;
-      }
+    $post_data = [
+      "id" => get_the_ID(),
+      "name" => get_the_title(),
+      "content" => get_the_content(),
+      "project_type" => $project_type,
+      "project_link" => get_field('project_link'),
+      "repo_link" => get_field('repo_link'),
+      "tech_stack" => get_field('tech_stack'),
+      "sketch" => get_field('sketch')
+    ];
 
-    } wp_reset_postdata();
+    switch ($project_type) {
+      case 'webapp':
+        array_push($webapp_projects, $post_data);
+        break;
+      case 'graphics':
+        array_push($graphics_projects, $post_data);
+        break;
+      case 'scripts':
+        array_push($script_projects, $post_data);
+        break;
+      case 'devops':
+        array_push($devops_projects, $post_data);
+        break;
+      default:
+        break;
+    }
+  }
+  wp_reset_postdata();
 
-    render_project('Web Applications', $webapp_projects);
-    render_project('Graphics', $graphics_projects);
-    render_project('Scripting', $script_projects);
-    render_project('DevOps', $devops_projects);
+  render_project('Web Applications', $webapp_projects);
+  render_project('Graphics', $graphics_projects);
+  render_project('Scripting', $script_projects);
+  render_project('DevOps', $devops_projects);
 
-    ?>
+  ?>
 
-  </main>
+</main>
 
 <?php get_footer(); ?>
