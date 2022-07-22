78205b2e957b946a3b3204f3d786dc6618442e58
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2022-07-22 16:58:01 +0200

    Add dark theme to blog posts on landing page. Add additional class to each post based on category.

--- header.php
+++ header.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <?php wp_head(); ?>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  </head>
-  <body>
+
+<head>
+  <?php wp_head(); ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
