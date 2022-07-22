98c0e038becc3d01d091ef8ea0b36320daf0ba0d
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2022-07-22 21:04:01 +0200

    Add ability to use prefers color scheme, and still toggle between dark/light theme

--- src/modules/themeToggler.ts
+++ src/modules/themeToggler.ts
@@ -27,7 +27,13 @@ export function addThemeToggler(checkboxId = 'theme-switcher') {
   /* Add change event to toggle dark class on body element */
 
   checkbox.addEventListener('change', () => {
-    document.body.classList.toggle('dark');
+    if (checkbox.checked) {
+      document.body.classList.add('dark');
+      document.body.classList.remove('light');
+    } else {
+      document.body.classList.add('light');
+      document.body.classList.remove('dark');
+    }
   });
 
   /* Check if prefers dark theme is true and immediately apply dark theme */
