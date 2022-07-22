21d14cf2a339656396ca47bf950d0c039d7603b3
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2022-07-22 12:09:55 +0200

    Updated class names to floating-menu

--- templates/floating_menu.php
+++ templates/floating_menu.php
@@ -1,12 +1,12 @@
-<aside class="site-options">
-  <ul class="site-options__menu">
-    <li class="site-options__item">
+<aside class="floating-menu">
+  <ul class="floating-menu__menu">
+    <li class="floating-menu__item">
       <a href="#">
         &uarr;
       </a>
     </li>
 
-    <li class="site-options__item">
+    <li class="floating-menu__item">
       <input type="checkbox" id="theme-switcher" />
       <label for="theme-switcher">
         <svg>
