d87ef80d410e593fdcfb7b90ca849d04189d3ed0
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2022-07-23 17:30:49 +0200

    Added new search icon; adjusted svg size to fit in 5rem without further rules

--- templates/floating_menu.php
+++ templates/floating_menu.php
@@ -1,9 +1,19 @@
 <aside class="floating-menu">
   <ul class="floating-menu__menu">
+
     <li class="floating-menu__item">
       <a href="#">
         &uarr;
       </a>
+    </li>
+
+    <li class="floating-menu__item">
+      <input type="checkbox" id="search-box" />
+      <label for="search-box">
+        <svg>
+          <use xlink:href="<?= get_theme_file_uri('assets/images/sprite.svg') . '#icon-search' ?>"></use>
+        </svg>
+      </label>
     </li>
 
     <!-- INSTERTED DYNAMICALLY WITH JS -->
