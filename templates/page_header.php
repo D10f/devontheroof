ff13c5d90d82035a19c674c817ad777537e35289
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2022-07-23 19:48:18 +0200

    remove searchbar from page header on blog and single post pages. Adjusted title size on smaller devices

--- templates/page_header.php
+++ templates/page_header.php
@@ -1,25 +1,20 @@
 <?php
 
-  if (is_single()) {
-    $nav_link_location = '/blog';
-    $nav_link_label = 'Back To Blog';
-  } else {
-    $nav_link_location = '/';
-    $nav_link_label = 'Back To Main';
-  }
+if (is_single()) {
+  $nav_link_location = '/blog';
+  $nav_link_label = 'Back To Blog';
+} else {
+  $nav_link_location = '/';
+  $nav_link_label = 'Back To Main';
+}
 ?>
 
 <header class="page-header">
   <h1 class="page-header__title">
-    <a
-      href="<?= site_url($nav_link_location)  ?>"
-      aria-label="<?= $nav_link_label ?>"
-    >
+    <a href="<?= site_url($nav_link_location)  ?>" aria-label="<?= $nav_link_label ?>">
       <small aria-label="hidden">&larr;</small>
       <?= $nav_link_label ?>
     </a>
   </h1>
 
-  <!-- Include searchbar  -->
-  <?php get_template_part('templates/searchbar'); ?>
 </header>
