cd2e86114b62f3cde49f510f265bd104d932ba1c
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2023-06-05 19:44:27 +0200

    Clean up and refactor

--- src/index.js
+++ src/index.js
@@ -4,52 +4,25 @@ import { SearchController } from "./modules/SearchController";
 import { TransitionEffect } from "./modules/TransitionEffect";
 import { FloatingMenuController } from "./modules/FloatingMenuController";
 import { Masonry } from "./modules/masonry";
-import { hasReducedMotion, isMobileDevice } from "./modules/utils";
 import { ThemeController } from "./modules/ThemeController";
+import { hasReducedMotion, isMobileDevice } from "./modules/utils";
 
 import "/styles/index.scss";
 
-function main() {
-  // Don't load on mobile devices, or when reduced motion is enabled
-  if (!isMobileDevice() && !hasReducedMotion()) {
-    new TransitionEffect(".section__title");
-  }
-
-  // Load a different set  of scripts depending on the visited page
-  if (window.location.pathname.startsWith("/blog")) {
-    loadBlogScripts();
-  } else if (window.location.pathname === "/") {
-    loadMainScripts();
-  } else {
-    loadSinglePostScripts();
-  }
-
-  // Always load scripts that apply to all pages
-  loadGlobalScripts();
+// Do not load on mobile devices of when reduced motion is enabled
+if (!isMobileDevice() && !hasReducedMotion()) {
+  new TransitionEffect('.section__title');
 }
 
-function loadMainScripts() {
+if (location.pathname.startsWith('/blog')) {
+  new Masonry('.blog__post-list');
+}
+
+if (location.pathname === '/') {
   new Highlighter(".highlight");
   new SketchController();
-  // pulseAnimation(".animate-pulse");
 }
 
-async function loadBlogScripts() {
-  new Masonry(".blog__post-list");
-}
-
-function loadSinglePostScripts() {
-  // postIndexToggler();
-  // new SearchController();
-  // new SinglePostController();
-  // pulseAnimation(".animate-pulse");
-}
-
-function loadGlobalScripts() {
-  new SearchController();
-  new ThemeController();
-  new FloatingMenuController();
-  // addThemeToggler();
-}
-
-main();
+new SearchController();
+new ThemeController();
+new FloatingMenuController();
