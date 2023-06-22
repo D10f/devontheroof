8e5469da3fe31566698244bb5a59bbb9fac47630
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2023-06-22 13:29:21 +0200

    Updated sidebar links to filter based on categories, similar to how archive.php would've done it but unnecessary for this simple view

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -2,7 +2,6 @@
 
 $categories = get_categories();
 $theme_uri = get_theme_file_uri('assets/images/sprite.svg');
-
 ?>
 
 <aside class="sidebar">
@@ -14,16 +13,24 @@ $theme_uri = get_theme_file_uri('assets/images/sprite.svg');
     </a>
     <p>Categories:</p>
     <menu class="sidebar__menu">
-        <?php
-        foreach ($categories as $category) { ?>
+        <?php foreach ($categories as $category) {
+
+            $is_active = is_category($category->name);
+        ?>
+
         <li class="sidebar__item">
-            <a class="sidebar__link sidebar__link--icon" href="/category/<?= $category->name ?>">
+            <a
+                class="sidebar__link sidebar__link--icon <?php echo $is_active ? 'sidebar__link--active' : ''; ?>"
+                href='<?php echo $is_active ? '/blog' : "/category/$category->name" ?>'
+            >
                 <svg>
                     <use xlink:href="<?= $theme_uri . "#icon-" . $category->name ?>" />
                 </svg>
                 <?= $category->name ?> (<?= $category->category_count ?>)
+                <?php echo $is_active ? '<span class="ml">&times;</span>' : '' ?>
             </a>
         </li>
+
         <?php } ?>
     </menu>
 </aside>
