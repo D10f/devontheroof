e77026e45f62b5d125da9514c181122bafe52f98
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2023-09-01 18:41:01 +0200

    Refactor landing page introduction text. Adjusted blog card icon size and hover transition.

--- front-page.php
+++ front-page.php
@@ -1,70 +1,80 @@
 <?php get_header(); ?>
 
-<aside class="navigation hide-on-phone">
-  <ul class="navigation__links">
-    <li class="navigation__item">
-      <a class="navigation__link" href="#web-applications">Web Apps</a>
-    </li>
-    <li class="navigation__item">
-      <a class="navigation__link" href="#graphics">Graphics</a>
-    </li>
-    <li class="navigation__item">
-      <a class="navigation__link" href="#scripting">Scripts</a>
-    </li>
-    <li class="navigation__item">
-      <a class="navigation__link" href="#devops">DevOps</a>
-    </li>
-    <li class="navigation__item">
-      <a class="navigation__link" href="#contact">Contact</a>
-    </li>
-  </ul>
-
-</aside>
+<!-- <aside class="navigation hide-on-phone"> -->
+<!--   <ul class="navigation__links"> -->
+<!--     <!-1- <li class="navigation__item"> -1-> -->
+<!--     <!-1-   <a class="navigation__link" href="#web-applications">Web Apps</a> -1-> -->
+<!--     <!-1- </li> -1-> -->
+<!--     <!-1- <li class="navigation__item"> -1-> -->
+<!--     <!-1-   <a class="navigation__link" href="#graphics">Graphics</a> -1-> -->
+<!--     <!-1- </li> -1-> -->
+<!--     <!-1- <li class="navigation__item"> -1-> -->
+<!--     <!-1-   <a class="navigation__link" href="#scripting">Scripts</a> -1-> -->
+<!--     <!-1- </li> -1-> -->
+<!--     <!-1- <li class="navigation__item"> -1-> -->
+<!--     <!-1-   <a class="navigation__link" href="#devops">DevOps</a> -1-> -->
+<!--     <!-1- </li> -1-> -->
+<!--     <li class="navigation__item"> -->
+<!--       <a class="navigation__link" href="#web-applications">Projects</a> -->
+<!--     </li> -->
+<!--     <li class="navigation__item"> -->
+<!--       <a class="navigation__link" href="/blog">Blog</a> -->
+<!--     </li> -->
+<!--     <li class="navigation__item"> -->
+<!--       <a class="navigation__link" href="#contact">About</a> -->
+<!--     </li> -->
+<!--   </ul> -->
+<!-- </aside> -->
 
 <header class="landing__header">
 
   <!-- Introduction -->
   <section class="landing__introduction">
+
     <?php the_content(); ?>
-    <ul>
-      <p>Who am I, you ask? I'm...</p>
-      <li class="landing__introduction--whoami">
-        <svg aria-hidden="true">
-          <use xlink:href="<?= get_theme_file_uri('assets/images/sprite.svg') . '#icon-code' ?>"></use>
-        </svg>
-        A Full-Stack Developer.
-      </li>
-      <li class="landing__introduction--whoami">
-        <svg aria-hidden="true">
-          <use xlink:href="<?= get_theme_file_uri('assets/images/sprite.svg') . '#icon-badge' ?>"></use>
-        </svg>
-        Linux Foundation Certified System Administrator.
-      </li>
-      <li class="landing__introduction--whoami">
-        <svg aria-hidden="true">
-          <use xlink:href="<?= get_theme_file_uri('assets/images/sprite.svg') . '#icon-cloud' ?>"></use>
-        </svg>
-        Passionate about all things web and cloud.
-      </li>
-      <li class="landing__introduction--whoami">
-        <svg aria-hidden="true">
-          <use xlink:href="<?= get_theme_file_uri('assets/images/sprite.svg') . '#icon-location' ?>"></use>
-        </svg>
-        Based in Spain.
-      </li>
-      <li class="landing__introduction--whoami">
-        <svg aria-hidden="true">
-          <use xlink:href="<?= get_theme_file_uri('assets/images/sprite.svg') . '#icon-talk' ?>"></use>
-        </svg>
-        Available, let's talk.
-      </li>
-    </ul>
+    <p class="landing__introduction--whoami">I'm a Web Developer from Spain who enjoys making and learning new things, especially in the realm of the web, retrogaming and open source technologies.</p>
+    <p class="landing__introduction--whoami">This website is where I write useful notes for my future self and things I find interesting about technology. I also use it to showcase some of my favorite projects that I've worked on.</p>
+    <p class="landing__introduction--whoami">Checkout some of my projects below, how I setup my development environment, my GitHub and Codeberg pages and get in touch.</p>
+
+    <!-- <ul> -->
+    <!--   <p>Who am I, you ask? I'm...</p> -->
+    <!--   <li class="landing__introduction--whoami"> -->
+    <!--     <svg aria-hidden="true"> -->
+    <!--       <use xlink:href="<?= get_theme_file_uri('assets/images/sprite.svg') . '#icon-code' ?>"></use> -->
+    <!--     </svg> -->
+    <!--     A Full-Stack Developer. -->
+    <!--   </li> -->
+    <!--   <li class="landing__introduction--whoami"> -->
+    <!--     <svg aria-hidden="true"> -->
+    <!--       <use xlink:href="<?= get_theme_file_uri('assets/images/sprite.svg') . '#icon-badge' ?>"></use> -->
+    <!--     </svg> -->
+    <!--     Linux Foundation Certified System Administrator. -->
+    <!--   </li> -->
+    <!--   <li class="landing__introduction--whoami"> -->
+    <!--     <svg aria-hidden="true"> -->
+    <!--       <use xlink:href="<?= get_theme_file_uri('assets/images/sprite.svg') . '#icon-cloud' ?>"></use> -->
+    <!--     </svg> -->
+    <!--     Passionate about all things web and cloud. -->
+    <!--   </li> -->
+    <!--   <li class="landing__introduction--whoami"> -->
+    <!--     <svg aria-hidden="true"> -->
+    <!--       <use xlink:href="<?= get_theme_file_uri('assets/images/sprite.svg') . '#icon-location' ?>"></use> -->
+    <!--     </svg> -->
+    <!--     Based in Spain. -->
+    <!--   </li> -->
+    <!--   <li class="landing__introduction--whoami"> -->
+    <!--     <svg aria-hidden="true"> -->
+    <!--       <use xlink:href="<?= get_theme_file_uri('assets/images/sprite.svg') . '#icon-talk' ?>"></use> -->
+    <!--     </svg> -->
+    <!--     Available, let's talk. -->
+    <!--   </li> -->
+    <!-- </ul> -->
   </section>
 
 
   <!-- Recent blog entries -->
   <section class="landing__recent-posts">
-    <a class="landing__posts-separator" href="<?= site_url('blog'); ?>">BLOG</a>
+    <a class="landing__posts-separator" id="blog-separator" href="<?= site_url('blog'); ?>">BLOG</a>
     <?php get_template_part('templates/recent_posts'); ?>
   </section>
 
@@ -124,6 +134,8 @@
   render_project('Graphics', $graphics_projects);
   render_project('Scripting', $script_projects);
   render_project('DevOps', $devops_projects);
+
+  get_template_part('templates/about');
 
   ?>
 
