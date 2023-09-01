a4fd72cccc42807731a3bff1e7aca191a94c0b30
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2023-09-01 18:41:48 +0200

    Removed letter transition effect

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import RaycastingSketch from './sketches/Raycasting.ts';
-import PathfindingSketch from './sketches/Pathfinding.ts';
-import CodeBlocksController from './modules/CodeBlocksController';
+import RaycastingSketch from "./sketches/Raycasting.ts";
+import PathfindingSketch from "./sketches/Pathfinding.ts";
+import CodeBlocksController from "./modules/CodeBlocksController";
 import { Highlighter } from "./modules/Highlighter";
 import { SearchController } from "./modules/SearchController";
 import { TransitionEffect } from "./modules/TransitionEffect";
@@ -13,17 +13,19 @@ import "/styles/index.scss";
 
 // Do not load on mobile devices or when reduced motion is enabled
 if (!isMobileDevice() && !hasReducedMotion()) {
-  new TransitionEffect('.section__title');
+  // new TransitionEffect(".section__title");
 }
 
 // Apply Masonry on post listing pages eg., blog, category, archives, etc.
 if (location.pathname.match(/^\/(blog|category)/)) {
-  new Masonry('.blog__post-list');
+  new Masonry(".blog__post-list");
 } else if (location.pathname.match(/^\/[\w-]+/)) {
-  document.addEventListener('DOMContentLoaded', CodeBlocksController, { once: true });
+  document.addEventListener("DOMContentLoaded", CodeBlocksController, {
+    once: true,
+  });
 }
 
-if (location.pathname === '/') {
+if (location.pathname === "/") {
   new Highlighter(".highlight");
   new RaycastingSketch();
   new PathfindingSketch();
