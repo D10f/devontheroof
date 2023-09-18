a0412e57034289fdae465a14db18cfbf779cf211
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2023-09-19 00:53:12 +0200

    Start work on about page with my development setup

--- templates/about.php
+++ templates/about.php
@@ -1,6 +1,28 @@
 <?php ?>
 
-<section class="section" id="about">
-  <header class="section__title">About Me</header>
-  <p>lorem ipsum</p>
+<section class="section section__about" id="about">
+  <header class="section__title mb-5">About</header>
+  <div class="section__about--content">
+    <h3>Operating System</h3>
+    <p>On my main machine I run <a href="https://www.debian.org">Debian 12</a>, as I hold stability and reliability as the most important aspects on a computer that is designed to get work done. The latest version of Debian has proven to be effective in bringing a great deal of balance between that and out of the box niceties.</p>
+
+    <p>I like to keep my desktop rather spartan-looking, favoring functionality over aesthetics. For that reason, I tend to use the <a href="https://www.gnome.org">GNOME</a> desktop environment, as it's quite complete with sensible defaults that work for me. <a href="https://xfce.org">XFCE</a> is another desktop environment that I like and use on low(er) end powered devices.</p>
+
+    <p>In terms of productivity, window managers look really promising. I'm currently testing a hybrid of XFCE + <a href="https://i3wm.org">i3wm</a>.</p>
+
+    <p>See: <a href="https://codeberg.org/d10f/ansible-desktop">how I setup my development machines using Ansible.</a></p>
+
+    <h3>Development Environment</h3>
+    <p>For things like running Docker containers, installing packages or pushing changes to a git repository, I prefer to use the terminal. Graphical user interfaces are fine but they are highly subjective about how they look; <em>modern</em> today means <em>ancient</em> tomorrow. In contrast, text-based interfaces are stable and tend to remain pretty similar for <em>decades</em>. Sometimes, they are the only option available when logging into remote servers.</p>
+    <p>As far as emulators go, I don't really have a preference but I've grown fond of <a href="https://github.com/gnunn1/tilix">Tilix</a> for it's builtin tab functionality. <a href="https://alacritty.org/">Alacritty</a> is another great choice that is really easy to configure. For purposes of compatibility I prefer using <a href="https://www.gnu.org/software/bash/">Bash</a> over "cooler" alternatives such as ZSH or Fish.</p>
+    <p><a href="https://neovim.io">Neovim</a> is my code editor of choice for most of my projects. It's pretty snappy and lightweight, very powerful and highly configurable although it takes some time getting used to it.</p>
+    <p>For anything else that Neovim can't do without spending some time configuring it, I resort to <a href="https://vscodium.com/">VSCodium</a>; that's the same as VSCode sans the Microsoft telemetry. One of my favorite builtin features is the markdown previewer.</p>
+
+    <p>See: <a href="https://codeberg.org/d10f/dotfiles">How I configure my programs.</a></p>
+
+    <h3>Theming</h3>
+    <p>If you are going to stare at something for hours every day, it's probably worth taking the time to make it look nice. Lately, I've been enjoying a nice pastel theme called <a href="https://github.com/catppuccin/catppuccin">Catppuccin</a> that I use everywhere, even on this very website! I really like the soft contrast and how easy it is on the eyes, especially its dark palette for when there's not enough light in the room at night.</p>
+    <p>There are times when theming can also improve your workflow by simply shifting things around. This is what I do in my custom <a href="https://www.mozilla.org/en-US/firefox/">Firefox</a> theme, by removing all the clutter that I don't need, and positioning what matters compactly to maximize screen real state.</p>
+
+  </div>
 </section>
