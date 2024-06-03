3e72ff430ca58637e706ea3d6bd31690ac9aa105
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-06-03 23:14:28 +0200

    Updated base path location for posts

--- src/lib/asciidoc/Parser.ts
+++ src/lib/asciidoc/Parser.ts
@@ -1,86 +1,86 @@
 import path from "path";
 import Asciidoctor, { Block, Document, Title } from "asciidoctor";
 import BaseConverter, {
-  CustomConverter,
+    CustomConverter,
 } from "@/lib/asciidoc/converters/BaseConverter";
 
-const BASE_PATHNAME = "public";
+const BASE_PATHNAME = "public/posts";
 
 export default class AsciidocParser {
-  private asciidoctor = Asciidoctor();
-  private document: Document;
-  private documentTitle: Title;
-  private converter: BaseConverter;
+    private asciidoctor = Asciidoctor();
+    private document: Document;
+    private documentTitle: Title;
+    private converter: BaseConverter;
 
-  constructor(private filename: string) {
-    this.converter = new BaseConverter();
-    this.registerConverter();
-    this.document = this.readFile(filename);
-    this.documentTitle = this.document.getDocumentTitle({
-      partition: true,
-    }) as Title;
-  }
+    constructor(private filename: string) {
+        this.converter = new BaseConverter();
+        this.registerConverter();
+        this.document = this.readFile(filename);
+        this.documentTitle = this.document.getDocumentTitle({
+            partition: true,
+        }) as Title;
+    }
 
-  get description() {
-    return this.document.getAttribute("description");
-  }
+    get description() {
+        return this.document.getAttribute("description");
+    }
 
-  get keywords() {
-    return this.document.getAttribute("keywords");
-  }
+    get keywords() {
+        return this.document.getAttribute("keywords");
+    }
 
-  /**
-   * A non-standard attribute list describing the technologies the document
-   * speaks about. It is later used to display an icon.
-   */
-  get technologies(): string[] {
-    const techs = this.document.getAttribute("technologies") || "";
-    return techs.split(" ");
-  }
+    /**
+     * A non-standard attribute list describing the technologies the document
+     * speaks about. It is later used to display an icon.
+     */
+    get technologies(): string[] {
+        const techs = this.document.getAttribute("technologies") || "";
+        return techs.split(" ");
+    }
 
-  get title() {
-    return this.documentTitle.getMain();
-  }
+    get title() {
+        return this.documentTitle.getMain();
+    }
 
-  get subtitle() {
-    return this.documentTitle.getSubtitle();
-  }
+    get subtitle() {
+        return this.documentTitle.getSubtitle();
+    }
 
-  get version() {
-    return this.document.getRevisionNumber();
-  }
+    get version() {
+        return this.document.getRevisionNumber();
+    }
 
-  get date() {
-    return this.document.getRevisionDate();
-  }
+    get date() {
+        return this.document.getRevisionDate();
+    }
 
-  get content() {
-    return this.document.getContent() as string;
-  }
+    get content() {
+        return this.document.getContent() as string;
+    }
 
-  get slug() {
-    return this.filename.replace(/\..*$/, "");
-  }
+    get slug() {
+        return this.filename.replace(/\..*$/, "");
+    }
 
-  get preamble() {
-    // https://gitlab.com/opendevise/oss/descriptionizer
-    const [documentBlock] = this.document.getBlocks() as Block[];
-    const [preambleBlock] = documentBlock.getBlocks() as Block[];
-    if (preambleBlock) return preambleBlock.getSourceLines()[0];
-  }
+    get preamble() {
+        // https://gitlab.com/opendevise/oss/descriptionizer
+        const [documentBlock] = this.document.getBlocks() as Block[];
+        const [preambleBlock] = documentBlock.getBlocks() as Block[];
+        if (preambleBlock) return preambleBlock.getSourceLines()[0];
+    }
 
-  private registerConverter() {
-    this.asciidoctor.ConverterFactory.register(this.converter, ["html5"]);
-  }
+    private registerConverter() {
+        this.asciidoctor.ConverterFactory.register(this.converter, ["html5"]);
+    }
 
-  private readFile(filename: string) {
-    return this.asciidoctor.loadFile(path.join(BASE_PATHNAME, filename), {
-      safe: "unsafe",
-    });
-  }
+    private readFile(filename: string) {
+        return this.asciidoctor.loadFile(path.join(BASE_PATHNAME, filename), {
+            safe: "unsafe",
+        });
+    }
 
-  use(converter: CustomConverter) {
-    this.converter.use(converter);
-    return this;
-  }
+    use(converter: CustomConverter) {
+        this.converter.use(converter);
+        return this;
+    }
 }
--- src/lib/asciidoc/posts.ts
+++ src/lib/asciidoc/posts.ts
@@ -3,33 +3,33 @@ import AsciidocParser from "./Parser";
 import { CustomConverter } from "./converters/BaseConverter";
 
 export function getPostFilenames() {
-  const files = fs.readdirSync("public", "utf-8");
-  return files.filter((file) => file.endsWith(".adoc"));
+    const files = fs.readdirSync("public/posts", "utf-8");
+    return files.filter((file) => file.endsWith(".adoc"));
 }
 
 export function getPostSlugs() {
-  return getPostFilenames().map((post) => ({
-    slug: post.replace(/\..*$/, ""),
-  }));
+    return getPostFilenames().map((post) => ({
+        slug: post.replace(/\..*$/, ""),
+    }));
 }
 
 export function getPostData(
-  filename: string,
-  converters: CustomConverter[] = [],
+    filename: string,
+    converters: CustomConverter[] = [],
 ) {
-  const post = new AsciidocParser(filename);
-  converters.forEach((converter) => post.use(converter));
-  return post;
+    const post = new AsciidocParser(filename);
+    converters.forEach((converter) => post.use(converter));
+    return post;
 }
 
 export function getAllPosts() {
-  return getPostFilenames().map((filename) => getPostData(filename));
+    return getPostFilenames().map((filename) => getPostData(filename));
 }
 
 export function generatePageMetadata(post: AsciidocParser) {
-  return {
-    title: post.title,
-    description: post.description,
-    keywords: post.keywords + ", " + post.technologies,
-  };
+    return {
+        title: post.title,
+        description: post.description,
+        keywords: post.keywords + ", " + post.technologies,
+    };
 }
