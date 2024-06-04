180ed9c9ef255a750f61bf7cd50950f2343ec10d
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-06-04 12:12:36 +0200

    Add shiki transformers to allow for source code annotation. Add custom transformer to process asciidoc's callout into a nicer output

--- src/lib/asciidoc/converters/CodeBlockConverter.ts
+++ src/lib/asciidoc/converters/CodeBlockConverter.ts
@@ -1,38 +1,86 @@
 import { Block } from "asciidoctor";
 import { CustomConverter } from "@/lib/asciidoc/converters/BaseConverter";
-import { BundledLanguage, BundledTheme, HighlighterGeneric } from "shiki";
+import {
+    BundledLanguage,
+    BundledTheme,
+    HighlighterGeneric,
+    ShikiTransformer,
+} from "shiki";
+import {
+    transformerNotationDiff,
+    transformerNotationHighlight,
+} from "@shikijs/transformers";
 
 type Highlighter = HighlighterGeneric<BundledLanguage, BundledTheme>;
 
+const htmlEntities = ["❶", "❷", "❸", "❹", "❺", "❻", "❼", "❽", "❾"];
+
 export default class CodeBlockConverter implements CustomConverter {
-  /**
-   * The node type that this class modifies.
-   */
-  public targetNode = "listing";
+    /**
+     * The node type that this class modifies.
+     */
+    public targetNode = "listing";
 
-  constructor(private highlighter: Highlighter) { }
+    constructor(private highlighter: Highlighter) {}
 
-  convert(node: Block) {
-    const input = node.getSourceLines().join("\n");
-    const lang = node.getAttribute("language");
-    const output = this.highlighter.codeToHtml(input, {
-      lang,
-      themes: {
-        light: "github-light",
-        dark: "github-dark",
-        latte: "catppuccin-latte",
-        frappe: "catppuccin-frappe",
-        macchiato: "catppuccin-macchiato",
-        mocha: "catppuccin-mocha",
-      },
-      defaultColor: false,
-      cssVariablePrefix: "--shiki-",
-    });
+    convert(node: Block) {
+        const input = node.getSourceLines().join("\n");
+        const lang = node.getAttribute("language");
+        const output = this.highlighter.codeToHtml(input, {
+            lang,
+            themes: {
+                light: "github-light",
+                dark: "github-dark",
+                latte: "catppuccin-latte",
+                frappe: "catppuccin-frappe",
+                macchiato: "catppuccin-macchiato",
+                mocha: "catppuccin-mocha",
+            },
+            transformers: [
+                this.transformAnnotations(),
+                transformerNotationDiff(),
+                transformerNotationHighlight(),
+            ],
+            defaultColor: false,
+            cssVariablePrefix: "--shiki-",
+        });
 
-    return `<div class="listingblock" data-language="${lang}">
-      <div class="content">
-        ${output}
-      </div>
-    </div>`;
-  }
+        return `<div class="listingblock" data-language="${lang}">
+            <div class="content">
+                ${output}
+            </div>
+        </div>`;
+    }
+
+    /**
+     * Replaces asciidoc's callout markers with nicer-looking numeric tags
+     */
+    private transformAnnotations(): ShikiTransformer {
+        return {
+            line(node) {
+                const lastNode = node.children.at(-1);
+                // @ts-ignore
+                const contents = lastNode?.children[0];
+                const hasCallout = contents?.value.match(/<(\d)+>\s*$/);
+
+                if (hasCallout) {
+                    const lastIdx = node.children.length - 1;
+
+                    node.children[lastIdx] = {
+                        type: "element",
+                        tagName: "span",
+                        properties: {
+                            class: "callout",
+                        },
+                        children: [
+                            {
+                                type: "text",
+                                value: htmlEntities[hasCallout[1] - 1],
+                            },
+                        ],
+                    };
+                }
+            },
+        };
+    }
 }
