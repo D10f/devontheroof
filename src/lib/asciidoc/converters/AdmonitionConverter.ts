a6a7f1e88372d0f0d44c2c3fa6be82647fccefa9
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-06-06 00:24:57 +0200

    Update: improve admonition converter and added styles for all types e.g., note, important, caution, etc. as well as svg icons

--- src/lib/asciidoc/converters/AdmonitionConverter.ts
+++ src/lib/asciidoc/converters/AdmonitionConverter.ts
@@ -10,17 +10,14 @@ export default class AdmonitionConverter implements CustomConverter {
     convert(node: AbstractNode): string {
         // @ts-ignore
         const content = node.lines;
-        switch (node.getAttribute("name")) {
-            case "warning":
-                return `
-                    <div class="admonition warning">
-                        <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z" />
-                        </svg>
-                        <p>${content}</p>
-                    </div>`;
-            default:
-                return "";
-        }
+        const type = node.getAttribute("name");
+
+        return `
+            <div class="admonition ${type}">
+                <svg xmlns="http://www.w3.org/2000/svg">
+                    <use href="/sprite.svg#icon-${type}" />
+                </svg>
+                <p>${content}</p>
+            </div>`;
     }
 }
