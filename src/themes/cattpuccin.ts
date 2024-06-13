8b3dd967fe9467f41fea3ec0f704bbd8805f210f
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-06-13 17:24:30 +0200

    Darkened (lightened for light theme) color of subtext

--- src/themes/cattpuccin.ts
+++ src/themes/cattpuccin.ts
@@ -1,51 +1,51 @@
 import { CSSColorProperty, CSSThemeProperty, ThemeStrategy } from ".";
 
 const variants = [
-  "Cattpuccin Latte",
-  "Cattpuccin Frappe",
-  "Cattpuccin Macchiato",
-  "Cattpuccin Mocha",
+    "Cattpuccin Latte",
+    "Cattpuccin Frappe",
+    "Cattpuccin Macchiato",
+    "Cattpuccin Mocha",
 ] as const;
 
 const colorMap: Record<CSSColorProperty, string> = {
-  red: "red",
-  orange: "peach",
-  yellow: "yellow",
-  green: "green",
-  blue: "sky",
-  purple: "mauve",
+    red: "red",
+    orange: "peach",
+    yellow: "yellow",
+    green: "green",
+    blue: "sky",
+    purple: "mauve",
 };
 
 const variableMap: Record<CSSThemeProperty, string> = {
-  "--bg-color": "crust",
-  "--bg-color-2": "mantle",
-  "--bg-color-3": "base",
-  "--text-color": "text",
-  "--subtext-color": "subtext1",
+    "--bg-color": "crust",
+    "--bg-color-2": "mantle",
+    "--bg-color-3": "base",
+    "--text-color": "text",
+    "--subtext-color": "subtext0",
 };
 
 export type CattpuccinVariants = (typeof variants)[number];
 
 export default class Cattpuccin implements ThemeStrategy {
-  constructor(public variant: CattpuccinVariants) { }
+    constructor(public variant: CattpuccinVariants) {}
 
-  get cssVariant() {
-    return this.variant.substring(11).toLowerCase();
-  }
+    get cssVariant() {
+        return this.variant.substring(11).toLowerCase();
+    }
 
-  get variants() {
-    return variants;
-  }
+    get variants() {
+        return variants;
+    }
 
-  get colors() {
-    return Object.keys(colorMap);
-  }
+    get colors() {
+        return Object.keys(colorMap);
+    }
 
-  cssThemeMap(property: CSSThemeProperty) {
-    return `--ctp-${this.cssVariant}-${variableMap[property]}`;
-  }
+    cssThemeMap(property: CSSThemeProperty) {
+        return `--ctp-${this.cssVariant}-${variableMap[property]}`;
+    }
 
-  cssColorMap(color: CSSColorProperty) {
-    return `--ctp-${this.cssVariant}-${colorMap[color]}`;
-  }
+    cssColorMap(color: CSSColorProperty) {
+        return `--ctp-${this.cssVariant}-${colorMap[color]}`;
+    }
 }
