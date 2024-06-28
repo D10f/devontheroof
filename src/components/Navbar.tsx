4c147626c00cc1bf1bbb4786982cf38f35ced96e
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-06-28 13:59:23 +0200

    Update: add functionality to hide navbar when scrolling downwards, and reappear when scrolling back upwards

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,17 @@
+"use client";
+
 import Link from "next/link";
 import ThemeController from "@/components/ThemeController";
 import NavbarBreadcrumb from "@/components/NavbarBreadcrumb";
+import useScrollDirection from "@/hooks/useScrollDirection";
 
 export default function Navbar() {
+    const { direction } = useScrollDirection();
+
+    const navbarStyle = direction > 0 ? "navbar navbar--hidden" : "navbar";
+
     return (
-        <nav className="navbar">
+        <nav className={navbarStyle}>
             <div className="navbar__content">
                 <h1>
                     <Link href="/">D10f</Link>
