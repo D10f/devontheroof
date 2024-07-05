6a7683f0343048f704da84d16492224ada840cec
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-07-05 02:45:24 +0200

    Update: add ability to edit max level heading shown in TOC through constructor argument

--- src/lib/asciidoc/converters/PreambleConverter.ts
+++ src/lib/asciidoc/converters/PreambleConverter.ts
@@ -4,58 +4,60 @@ import { CustomConverter } from "@/lib/asciidoc/converters/BaseConverter";
 type TocPosition = "before" | "after";
 
 type TocNode = {
-  title: string;
-  id: string;
-  level: number;
-  children: TocNode[];
+    title: string;
+    id: string;
+    level: number;
+    children: TocNode[];
 };
 
 export default class PreambleConverter implements CustomConverter {
-  public targetNode = "preamble";
+    public targetNode = "preamble";
 
-  convert(node: AbstractNode, defaultConverter: Converter) {
-    const toc = node
-      .getDocument()
-      .getSections()
-      .map((node) => this.buildToc(node))
-      .map((node) => this.buildTocHtml(node))
-      .join("");
+    constructor(public readonly tocMaxLevel = 3) {}
 
-    const preamble = defaultConverter.convert(node);
+    convert(node: AbstractNode, defaultConverter: Converter) {
+        const toc = node
+            .getDocument()
+            .getSections()
+            .map((node) => this.buildToc(node))
+            .map((node) => this.buildTocHtml(node))
+            .join("");
 
-    return preamble + '<menu class="toc">' + toc + "</menu>";
-  }
+        const preamble = defaultConverter.convert(node);
 
-  private buildToc(node: Section, maxLevel = 3): TocNode {
-    const tocNode = {
-      title: node.getTitle() as string,
-      id: node.getId() as string,
-      level: node.getLevel(),
-      children: [],
-    };
-
-    if (tocNode.level === maxLevel) {
-      return tocNode;
+        return preamble + '<menu class="toc">' + toc + "</menu>";
     }
 
-    return {
-      ...tocNode,
-      children: node.getSections().map((node) => this.buildToc(node)),
-    };
-  }
+    private buildToc(node: Section, maxLevel = MAX_LEVEL): TocNode {
+        const tocNode = {
+            title: node.getTitle() as string,
+            id: node.getId() as string,
+            level: node.getLevel(),
+            children: [],
+        };
 
-  private buildTocHtml(node: TocNode): string {
-    const liTag = '<li class="toc__item">';
-    const link = `<a href="#${node.id}">${node.title}</a>`;
+        if (tocNode.level === maxLevel) {
+            return tocNode;
+        }
 
-    if (node.children.length === 0) {
-      return liTag + link + "</li>";
+        return {
+            ...tocNode,
+            children: node.getSections().map((node) => this.buildToc(node)),
+        };
     }
 
-    const childNodesHtml = node.children
-      .map((n) => this.buildTocHtml(n))
-      .join("");
+    private buildTocHtml(node: TocNode): string {
+        const liTag = '<li class="toc__item">';
+        const link = `<a href="#${node.id}">${node.title}</a>`;
 
-    return liTag + link + "<ul>" + childNodesHtml + "</ul></li>";
-  }
+        if (node.children.length === 0) {
+            return liTag + link + "</li>";
+        }
+
+        const childNodesHtml = node.children
+            .map((n) => this.buildTocHtml(n))
+            .join("");
+
+        return liTag + link + "<ul>" + childNodesHtml + "</ul></li>";
+    }
 }
