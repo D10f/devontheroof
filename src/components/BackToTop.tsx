f179b5ca010f452af87cf0ff0da836f6b13b78fd
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-07-08 14:48:56 +0200

    Add a 'scroll back to top' button

---
+++ src/components/BackToTop.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useMemo } from "react";
+import { FaChevronUp } from "react-icons/fa6";
+import useScrollDirection from "@/hooks/useScrollDirection";
+
+export default function BackToTop() {
+    const { direction } = useScrollDirection();
+    const hideBtn = direction > 0;
+
+    const btnStyle = useMemo(() => {
+        const baseStyles = "btn btn--round btn--float ";
+        return baseStyles + (hideBtn ? "btn--hidden" : "");
+    }, [hideBtn]);
+
+    function scrollUp() {
+        document.body.scrollIntoView({ behavior: "smooth" });
+    }
+
+    return (
+        <button className={btnStyle} onClick={scrollUp}>
+            <FaChevronUp />
+        </button>
+    );
+}
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,81 +1,57 @@
 import Link from "next/link";
 import Tooltip from "@/components/Tooltip";
+import BackToTop from "@/components/BackToTop";
 
 export default function Footer() {
     return (
         <footer className="footer">
-            <h2>Thanks for visiting!</h2>
+            <BackToTop />
+
             <div className="footer__field">
-                <p>You can find more about me on:</p>
+                <p>Built with:</p>
                 <ul>
                     <li>
                         <Link
-                            aria-label="Link to Codeberg profile"
-                            href="https://codeberg.org/D10f"
-                            data-tooltip-id="codeberg"
+                            aria-label="Link to send an email to address"
+                            data-tooltip-id="_nextjs"
+                            href="https://nextjs.org"
                         >
                             <svg aria-hidden="true">
-                                <use href={`/sprite.svg#icon-codeberg`} />
+                                <use href={`/sprite.svg#icon-nextjs`} />
                             </svg>
                         </Link>
-                        <Tooltip id="codeberg" place="top" content="Codeberg" />
+                        <Tooltip id="_nextjs" place="top" content="Next.js" />
                     </li>
                     <li>
                         <Link
-                            aria-label="Link to Github profile"
-                            href="https://github.com/D10f"
-                            data-tooltip-id="github"
+                            aria-label="Link to send an email to address"
+                            data-tooltip-id="_asciidoc"
+                            href="https://asciidoctor.org"
+                        >
+                            <svg aria-hidden="true">
+                                <use href={`/sprite.svg#icon-asciidoc`} />
+                            </svg>
+                        </Link>
+                        <Tooltip
+                            id="_asciidoc"
+                            place="top"
+                            content="AsciiDoc"
+                        />
+                    </li>
+                    <li>
+                        <Link
+                            aria-label="Link to send an email to address"
+                            data-tooltip-id="_github"
+                            href="https://github.com/d10f/devontheroof"
                         >
                             <svg aria-hidden="true">
                                 <use href={`/sprite.svg#icon-github`} />
                             </svg>
                         </Link>
-                        <Tooltip id="github" place="top" content="GitHub" />
-                    </li>
-                    <li>
-                        <Link
-                            aria-label="Link to Codepen profile"
-                            href="https://codepen.io/D10f"
-                            data-tooltip-id="codepen"
-                        >
-                            <svg aria-hidden="true">
-                                <use href={`/sprite.svg#icon-codepen`} />
-                            </svg>
-                        </Link>
-                        <Tooltip id="codepen" place="top" content="Codepen" />
-                    </li>
-                </ul>
-            </div>
-
-            <div className="footer__field">
-                <p>Get in touch:</p>
-                <ul>
-                    <li>
-                        <Link
-                            aria-label="Link to send an email to address devontheroof@pm.me"
-                            data-tooltip-id="email"
-                            href="mailto:devontheroof@pm.me"
-                        >
-                            <svg aria-hidden="true">
-                                <use href={`/sprite.svg#icon-email`} />
-                            </svg>
-                        </Link>
-                        <Tooltip id="email" place="top" content="Email" />
-                    </li>
-                    <li>
-                        <Link
-                            aria-label="Link to a public PGP key to encrypt the body of an email"
-                            data-tooltip-id="gpg"
-                            href="#"
-                        >
-                            <svg aria-hidden="true">
-                                <use href={`/sprite.svg#icon-gpg-key`} />
-                            </svg>
-                        </Link>
                         <Tooltip
-                            id="gpg"
+                            id="_github"
                             place="top"
-                            content="PGP public key"
+                            content="Source Code"
                         />
                     </li>
                 </ul>
@@ -83,3 +59,81 @@ export default function Footer() {
         </footer>
     );
 }
+
+//<h2>Thanks for visiting!</h2>
+
+//<div className="footer__field">
+//    <p>You can find more about me on:</p>
+//    <ul>
+//        <li>
+//            <Link
+//                aria-label="Link to Codeberg profile"
+//                href="https://codeberg.org/D10f"
+//                data-tooltip-id="codeberg"
+//            >
+//                <svg aria-hidden="true">
+//                    <use href={`/sprite.svg#icon-codeberg`} />
+//                </svg>
+//            </Link>
+//            <Tooltip id="codeberg" place="top" content="Codeberg" />
+//        </li>
+//        <li>
+//            <Link
+//                aria-label="Link to Github profile"
+//                href="https://github.com/D10f"
+//                data-tooltip-id="github"
+//            >
+//                <svg aria-hidden="true">
+//                    <use href={`/sprite.svg#icon-github`} />
+//                </svg>
+//            </Link>
+//            <Tooltip id="github" place="top" content="GitHub" />
+//        </li>
+//        <li>
+//            <Link
+//                aria-label="Link to Codepen profile"
+//                href="https://codepen.io/D10f"
+//                data-tooltip-id="codepen"
+//            >
+//                <svg aria-hidden="true">
+//                    <use href={`/sprite.svg#icon-codepen`} />
+//                </svg>
+//            </Link>
+//            <Tooltip id="codepen" place="top" content="Codepen" />
+//        </li>
+//    </ul>
+//</div>
+//
+//<div className="footer__field">
+//    <p>Get in touch:</p>
+//    <ul>
+//        <li>
+//            <Link
+//                aria-label="Link to send an email to address devontheroof@pm.me"
+//                data-tooltip-id="email"
+//                href="mailto:devontheroof@pm.me"
+//            >
+//                <svg aria-hidden="true">
+//                    <use href={`/sprite.svg#icon-email`} />
+//                </svg>
+//            </Link>
+//            <Tooltip id="email" place="top" content="Email" />
+//        </li>
+//        <li>
+//            <Link
+//                aria-label="Link to a public PGP key to encrypt the body of an email"
+//                data-tooltip-id="gpg"
+//                href="#"
+//            >
+//                <svg aria-hidden="true">
+//                    <use href={`/sprite.svg#icon-gpg-key`} />
+//                </svg>
+//            </Link>
+//            <Tooltip
+//                id="gpg"
+//                place="top"
+//                content="PGP public key"
+//            />
+//        </li>
+//    </ul>
+//</div>
