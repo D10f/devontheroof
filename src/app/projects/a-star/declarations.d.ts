97e2ede8a615ad51d636ba1c58a630326cb6e7a6
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2024-07-12 12:33:14 +0200

    Add project page for a* star visualization. For lack of a better option (for now anyway), assets are included in public dir

---
+++ src/app/projects/a-star/declarations.d.ts
@@ -0,0 +1,8 @@
+// declarations.d.ts - place it anywhere, this is an ambient module
+declare namespace JSX {
+    interface IntrinsicElements {
+        "tile-picker": any;
+    }
+}
+
+declare module "@d10f/a-star-viz";
