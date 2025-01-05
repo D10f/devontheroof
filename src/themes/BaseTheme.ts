a0c2e56b82da36a34d58a23152ef913cdd1c2551
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2025-01-05 20:41:42 +0100

    add codeblock updater

--- src/themes/BaseTheme.ts
+++ src/themes/BaseTheme.ts
@@ -51,4 +51,25 @@ export class BaseTheme {
     getProperty(prop: ThemeCSSProps) {
         return `--${this.cssPrefix}-${this.variant}-${this.variant.get(prop)}`;
     }
+
+    updateCodeBlockProps() {
+        const codeblocks = document.getElementsByClassName("shiki");
+
+        for (let i = 0, l = codeblocks.length; i < l; ++i) {
+            const codeblock = codeblocks[i] as HTMLElement;
+            codeblock.style.setProperty(
+                "background-color",
+                `var(--shiki-${this.name.toLowerCase()}-${this.variant}-bg)`,
+            );
+
+            const tokens = codeblock.getElementsByTagName("span");
+            for (let j = 0, l = tokens.length; j < l; ++j) {
+                const token = tokens[j] as HTMLElement;
+                token.style.setProperty(
+                    "color",
+                    `var(--shiki-${this.name.toLowerCase()}-${this.cssVariant})`,
+                );
+            }
+        }
+    }
 }
