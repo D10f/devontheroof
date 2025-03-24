8c1227aa2bccac678566b20b5cea701e3c771b4e
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2025-03-24 15:21:08 -0400

    Update admonition (side notes) position on either side of the post, alternating positions

--- src/lib/asciidoc/converters/AdmonitionConverter.ts
+++ src/lib/asciidoc/converters/AdmonitionConverter.ts
@@ -7,14 +7,25 @@ export default class AdmonitionConverter implements CustomConverter {
      */
     public targetNode = "admonition";
 
+    /**
+     * Keeps a count of how many admonition have been processed. This is
+     * used to assign them a HTML class to position them left/right in
+     * alternative order.
+     */
+    private admonitionCounter = 0;
+
     convert(node: AbstractNode): string {
         // @ts-ignore
         const lines = node.lines;
         const type = node.getAttribute("name");
         const content = lines.join("").replace(/`([^`]+)`/g, "<code>$1</code>");
+        let position =
+            this.admonitionCounter++ % 2 === 0
+                ? "admonition--left"
+                : "admonition--right";
 
         return `
-            <div class="admonition ${type}">
+            <div class="admonition ${position} ${type}">
                 <svg xmlns="http://www.w3.org/2000/svg">
                     <use href="/sprite.svg#icon-${type}" />
                 </svg>
