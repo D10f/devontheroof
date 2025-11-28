0c1044ff31e8a9adb205e711dec40f42bcc4111d
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2025-11-28 09:51:31 -0500

    Remove Next.js code - prepare for Astro rewrite

--- src/app/projects/a-star/declarations.d.ts
+++
@@ -1,8 +0,0 @@
-// declarations.d.ts - place it anywhere, this is an ambient module
-declare namespace JSX {
-    interface IntrinsicElements {
-        "a-star-viz": any;
-    }
-}
-
-declare module "@d10f/a-star-viz";
--- src/hooks/useScroll.ts
+++
@@ -1,9 +0,0 @@
-import { useEventListener } from "usehooks-ts";
-import throttle from "@/lib/utils/throttle";
-
-export default function useScroll(
-    callback: (e: Event) => void,
-    throttleTime = 100,
-) {
-    useEventListener("scroll", throttle(callback, throttleTime));
-}
