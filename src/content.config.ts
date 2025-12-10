b99890fbe95dd6885b4efaa573099823789aa3d3
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2025-12-10 09:06:01 -0500

    Update Content collection config

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,22 +1,19 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from 'astro:content';
 import { asciidocLoader } from '@d10f/asciidoc-astro-loader';
 
 const blog = defineCollection({
-    loader: asciidocLoader({
-        base: 'src/content/blog',
-        syntaxHighlighting: {
-            theme: {
-                light: 'catppuccin-latte',
-                dark: 'catppuccin-frappe'
-            }
-        }
-    }),
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        preamble: z.string().optional(),
-        createdAt: z.coerce.date()
-    })
+	loader: asciidocLoader({
+		base: 'src/content/blog',
+		syntaxHighlighting: {
+			theme: 'catppuccin-frappe',
+		},
+	}),
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		preamble: z.string().optional(),
+		createdAt: z.coerce.date(),
+	}),
 });
 
 export const collections = { blog };
