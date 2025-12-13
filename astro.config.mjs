5f915bab63d110419f32b0025c22a5789568149e
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2025-12-12 20:26:10 -0500

    Add Alpine.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [alpinejs()],
+});
